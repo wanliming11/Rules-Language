537dbcf3a00907eb328b3b1346451c9c30ce1ef7
万力铭 <wanliming@douyu.tv>
2023-02-25 23:00:08 +0800

    init proj

---
+++ rulesLang/rulesLang/rulesLangApp.swift
@@ -0,0 +1,17 @@
+//
+//  rulesLangApp.swift
+//  rulesLang
+//
+//  Created by Murloc Wan on 2023/2/25.
+//
+
+import SwiftUI
+
+@main
+struct rulesLangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
